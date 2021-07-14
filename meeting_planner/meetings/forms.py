bcd46d7f5563443d4f2228c3a7f91cfc892e3103
Michael Martin <mike.martin@verizondigitalmedia.com>
2021-07-14 14:29:41 -0700

    Finished tutorial

---
+++ meeting_planner/meetings/forms.py
@@ -0,0 +1,27 @@
+from datetime import date
+
+from django.forms import ModelForm
+from django.forms import DateInput
+from django.forms import TimeInput
+from django.forms import TextInput
+from django.forms import IntegerField
+from django.core.exceptions import ValidationError
+
+from .models import Meeting
+
+
+class MeetingForm(ModelForm):
+    class Meta:
+        model = Meeting
+        fields = '__all__'
+        widgets = {
+            'date': DateInput(attrs={"type": "date"}),
+            'start': TimeInput(attrs={"type": "time"}),
+            'duration': TextInput(attrs={"type": "number", "min": "1", "max": "4"})
+        }
+
+    def clean_date(self):
+        d =self.cleaned_data.get("date")
+        if d < date.today():
+            raise ValidationError("Meeting cannot be in the past")
+        return d
--- meeting_planner/meetings/views.py
+++ meeting_planner/meetings/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 from django.shortcuts import get_object_or_404
+# from django.forms import modelform_factory
 from .models import Meeting
 from .models import Room
 # Create your views here.
+from .forms import MeetingForm
+
 
 def detail(request, id):
     meeting = get_object_or_404(Meeting, pk=id)
@@ -12,3 +16,27 @@ def detail(request, id):
 def rooms_list(request):
     rooms = Room.objects.all()
     return render(request, "meetings/rooms_list.html", {"rooms": rooms})
+
+
+# MeetingForm = modelform_factory(Meeting, exclude=[])
+# def new(request):
+#     if request.method == "POST":
+#         form = MeetingForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect("welcome")
+#     else:
+#         form = MeetingForm()
+#     return render(request, "meetings/new.html", {"form": form})
+
+
+
+def new(request):
+    if request.method == "POST":
+        form = MeetingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("welcome")
+    else:
+        form = MeetingForm()
+    return render(request, "meetings/new.html", {"form": form})
